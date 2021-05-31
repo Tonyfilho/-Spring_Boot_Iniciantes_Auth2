ac9fe18fd3bed2b57de413de24e62c00221cebd5
tony filho <antonio.m.filho>
2021-05-31 10:35:52 +0100

    add starts, and conf of application and security class

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # -Spring_Boot_Iniciantes_Auth2
+# Este repositorio é somente para configuração do Spring OAuth2 Resource Service.
+# O Start Resource Service, permite o Spring Receber/Entender TOKENs.
+# ###########Existem 3 Starts para Spring Security####################
+# 1º OAuth2 Client que é  um client que receberá um TOKEN de outra API
+# 2º OAuth2 Resource Service Que é o nosso Caso, onde temos um Servidor JBoss/Keycloak que nos fornece os TOKENs
+# 3º Cloud OAuth2 que é um client em nuvem.
+# Obs: Esta class receberá TOKEN NÃO será um Authorization Service, que neste caso aqui é KeyCloak
+# ################ Usaremos a OAuth2 Resource Service################
+# E e sua class de Configuração que extende um interface 
+# O Pom.xml e suas configurações para ter acesso ao Servidor Jboss/KeyCloak startado em Docker.
+# E Configurar o Application Services
--- src/main/java/spring/com/spring_iniciantes/security/ApplicationSecurityConfig.java
+++ src/main/java/spring/com/spring_iniciantes/security/ApplicationSecurityConfig.java
@@ -1,10 +1,12 @@
 package spring.com.spring_iniciantes.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration // Tem que ser um tipo de class do spring de configuração
 @EnableWebSecurity // esta é a anotação para configuração de segurança
@@ -12,12 +14,29 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
-        HttpBasicConfigurer<HttpSecurity> httpBasic = http.authorizeRequests()// Autorizar o Request
-                .anyRequest()// Todos Request
-                .authenticated() // São Autendicados
-                .and() // E autenticados COM??
-                .httpBasic(); // HTTP BASIC
+        http.cors()
+        .and()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/")
+        .hasAuthority("User")
+        .anyRequest()
+        .authenticated()
+        .and()
+        .oauth2ResourceServer()
+        .jwt()
+        .jwtAuthenticationConverter(getjwtAuthenticationConverter());
+        
     }
-}
-// end class
+
+    // este Metodo EDITA a opção default do JwaAuthentication , desta forma teremos acesso a uma Var chamada authorities
+
+    JwtAuthenticationConverter getjwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("authorities");
+        converter.setAuthorityPrefix("");
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authenticationConverter;
+    }
+    
+}// end class
